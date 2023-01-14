7f67105a0afe20ee86b3987d2d3ccd9f25b8d431
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-01-14 17:54:19 +0400

    13

---
+++ Basic JavaScript/const myMusic = [.js
@@ -0,0 +1,25 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  
+  myMusic.push({
+     "artist": "Bitlly Joel",
+      "title": "Piaeno Man",
+      "release_year": 193,
+      "formats": [
+        "CDr",
+        "8Te",
+        "LPr"
+      ],
+      "gold": true
+  })
