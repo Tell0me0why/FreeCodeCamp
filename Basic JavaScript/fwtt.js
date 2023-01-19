61ef5544fe40ddf4a586ccb18f33ca09a4c42875
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-01-19 17:36:25 +0400

    qaz

---
+++ Basic JavaScript/fwtt.js
@@ -0,0 +1,48 @@
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+    let answer1 = '';
+  for (let i = 0; i < contacts.length(); i++) {
+    if (contacts[i].hasOwnProperety(name)) {
+        if (contacts[i].hasOwnProperety(prop)) {
+            return contacts[i][prop];
+        } else {
+            return "No such property"
+        }
+
+    } else {
+        answer1 = "No such contact"
+    }
+  }
+  return answer1;
+    // Only change code above this line
+  }
+  
+  lookUpProfile("Kristian", "lastName")
