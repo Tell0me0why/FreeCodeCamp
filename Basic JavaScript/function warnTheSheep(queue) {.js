fcfb8304d40cc5429b4a21a6c5d660a21e55b91d
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-01-20 14:48:23 +0400

    d

--- Basic JavaScript/function warnTheSheep(queue) {.js
+++ Basic JavaScript/function warnTheSheep(queue) {.js
@@ -4,7 +4,7 @@ function warnTheSheep(queue) {
         if (i == queue.length -1) {
           return "Pls go away and stop eating my sheep"
         } else {
-          return "Oi! Sheep number "  +  [queue.length - i] + "! You are about to be eaten by a wolf!" 
+          return "Oi! Sheep number "  +  [queue.length - i - 1] + "! You are about to be eaten by a wolf!" 
         }
       }
     }
