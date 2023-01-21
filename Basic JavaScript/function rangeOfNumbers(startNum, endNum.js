a4ccceb0706f241bc8433cd8afb5802c75466311
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-01-21 12:38:13 +0400

    dg

--- Basic JavaScript/function rangeOfNumbers(startNum, endNum.js
+++ Basic JavaScript/function rangeOfNumbers(startNum, endNum.js
@@ -1,8 +1,8 @@
 function rangeOfNumbers(startNum, endNum) {
-    if (endNum < startNum) {
+    if (endNum - startNum < 0) {
       return [];
     } else {
-      let arr = rangeOfNumbers(endNum-1);
+      let  arr = rangeOfNumbers(startNum, endNum--);
       arr.push(endNum);
       return arr;
   
