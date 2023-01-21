d27303d38b2842b1fba9bedd336f250eb06f743e
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-01-21 19:21:07 +0400

    y

--- Basic JavaScript/wer.js
+++ Basic JavaScript/wer.js
@@ -8,20 +8,21 @@ function alphabet(ns) {
  let k1 = 0;
  let k2 = 0;
  for (let i = 0; i < 8; ++i) {
-    if (ns[i] == ns[0] * ns[1] || ns[i] == ns[2] * ns[1]) {
+    if (ns[i] == ns[0] * ns[1] || ns[i-1] == ns[2] * ns[1]) {
         if (k1 == k2) {
             k1 = i;
+            ns.splice(k1, 1);
         } else {
-            k2 = i;
+            k2 = i - 1;
         }
     }
     
  }
- ns.splice(k1 , 1);
- ns.splice(k2 - 1, 1);
+ 
+ ns.splice(k2 , 1);
  console.log(ns);
  return ns[3];
    
    }
 
- console.log(alphabet([2, 5, 20, 21,   10, 100, 420, 42]));
+ console.log(alphabet([360,1,24,750,750,15,18000,15]));
