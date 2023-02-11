8c0b46ae88a108ce198ac4f958226b0382c62589
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-02-11 13:03:29 +0400

    task 9

---
+++ ES6/9.js
@@ -0,0 +1,11 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const {tomorrow, today} = HIGH_TEMPERATURES;
+  
+  // Only change code above this line
