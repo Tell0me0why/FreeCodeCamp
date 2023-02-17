721040180596fe592a24ed568546128131977824
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-02-17 18:01:47 +0400

    task 17

--- ES6/14.js
+++ ES6/14.js
@@ -3,9 +3,9 @@ const stats = {
     standard_deviation: 4.34,
     median: 34.54,
     mode: 23.87,
-    min: -0.75,
-    average: 35.85
+    min: -0.795,
 };
+let b = 2;
 
 // Only change code below this line
 const half = ({max, min}) => (max + min) / 2.0;
