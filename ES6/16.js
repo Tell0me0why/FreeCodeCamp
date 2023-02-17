b9150fe2d7a15c2b49fd131ebb15ddb387f2982d
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-02-17 15:50:19 +0400

    task 16

---
+++ ES6/16.js
@@ -0,0 +1,10 @@
+const createPerson = (name, age, gender) => ({
+    // Only change code below this line
+
+    name,
+    age,
+    gender
+
+    // Only change code above this line
+});
+// look at '(' and ')' without them code won't work
