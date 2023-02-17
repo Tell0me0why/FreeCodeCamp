136ce5583be10b3f06fbed60f8cbf3f5a257e884
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-02-17 15:37:01 +0400

    right task 15

--- ES6/15.js
+++ ES6/15.js
@@ -4,14 +4,14 @@ const result = {
     skipped: ["no-extra-semi", "no-dup-keys"]
 };
 function makeList(arr) {
-    // Only change code below this line
+    "use strict";
+    // change code below this line
     const failureItems = [];
-    for (let i = 0; i < arr.length(); ++i) {
-        failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+    for (let i = 0; i < arr.length; i++) {
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
     }
-        // Only change code above this line
-
-        return failureItems;
+    // change code above this line
+    return failureItems;
 }
 
 const failuresList = makeList(result.failure);
