15b8e3dd24350f02bd3714cd23626755ee45d6c5
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-02-26 16:50:09 +0400

    task 26

--- ES6/26.js
+++ ES6/26.js
@@ -17,5 +17,5 @@ The syntax differs in one key place.
 The imported value, add, is not surrounded by curly braces ({}).
 add here is simply a variable name for whatever the default export
  of the math_functions.js file is. You can use any name
- here when importing a default.
+ here when importing a default./
  */
