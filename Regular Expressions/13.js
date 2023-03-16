14418d32234fd169c95ee23de0c97e0e4716adbf
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-03-16 09:45:15 +0400

    Find Characters with Lazy Matching

--- Regular Expressions/13.js
+++ Regular Expressions/13.js
@@ -1,8 +1,10 @@
+chewieQuote = 'Aaaaaaaaaaaaaaaarrrgh!'
 // Only change code below this line
 let chewieRegex = /Aa*/; // Change this line
 // Only change code above this line
 
 let result = chewieQuote.match(chewieRegex);
+console.log(result)
 /*
 The last challenge used the plus + sign to look
  for characters that occur one or more times.
