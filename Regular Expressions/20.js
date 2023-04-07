92aa26779550b59e46113ea693634baabd316af2
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-04-07 11:00:06 +0400

    Match All Non-Numbers

--- Regular Expressions/20.js
+++ Regular Expressions/20.js
@@ -1,6 +1,7 @@
 let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/g; // Change this line
 let result = movieName.match(numRegex).length;
+console.log(result)
 /*
 You've learned shortcuts for common string patterns
  like alphanumerics. Another common pattern is
