240ab6135ef4e59e75abc288342796be897bfd12
Heincrich Schmidt <schmidt0heinrich@gmail.com>
2023-05-01 16:21:59 +0400

    Catch Missing Open and Closing Parenthesis After a Function Call

---
+++ Debugging/8.js
@@ -0,0 +1,17 @@
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+  console.log(result);
+  /**
+   * function myFunction() {
+  return "You rock!";
+}
+let varOne = myFunction;
+let varTwo = myFunction();
+Here varOne is the function myFunction, 
+and varTwo is the string You rock!.
+   */
